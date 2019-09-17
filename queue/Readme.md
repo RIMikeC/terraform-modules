d3ba49b7580df8f85956743994d4b0e14f07c19c
RIMikeC <mike.charles@river-island.com>
2019-09-17 13:13:32 +0100

    Add new SQS module

---
+++ queue/Readme.md
@@ -0,0 +1,4 @@
+## Queue
+Adds a queue, the name for which is passed in as a parameter
+
+For use with Terraform 0.12.8+
---
+++ queue/variables.tf
@@ -0,0 +1,5 @@
+variable "queue_name" {
+  description = "Optional name of queue to create"
+  type        = "string"
+  default     = "dummy"
+}
